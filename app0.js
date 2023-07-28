f2087087d93e6006429b2fd751fca4ce18c8a0e7
marysilisque <silisquealejandra3@gmail.com>
2023-07-28 15:13:35 -0300

    mi primer commit

---
+++ app0.js
@@ -0,0 +1,7 @@
+// variables
+// productos js.
+var electrodomesticos = "celular"; "lavaropas"; "licuadora";
+var muebles = "ropero"; "mesa";
+var precio = "50000"; "18000"; "34000";
+var electrodomesticos_y_precio = electrodomesticos + " " + precio; 
+console.log(electrodomesticos, muebres, precio);
